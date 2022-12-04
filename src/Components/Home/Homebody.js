0f09d5c4a46c8e0751f8095bd75ecd727f10461c
Pradeeppss <pradeeppssosp@gmail.com>
2022-12-04 21:23:36 +0530

    deployment change again

--- src/Components/Home/Homebody.js
+++ src/Components/Home/Homebody.js
@@ -8,7 +8,7 @@ function Homebody() {
   const [hotels, sethotels] = useState([]);
 
   let getallhotels = async () => {
-    let URI = "https://airbnb-clone-api-pradeep.herokuapp.com/getallhotels";
+    let URI = "https://airbnbapp.up.railway.app/getallhotels";
     let { data } = await axios.get(URI);
     // console.log(data);
     sethotels([...data.result]);
